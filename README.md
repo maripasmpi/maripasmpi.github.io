99802d2168202cf14e80ed66a6bf08a271da1b61
maripasmpi <syifaafzyh03@gmail.com>
2024-05-05 05:27:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# maripasmpi.github.io
---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5uj3velklUS":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.top.close();
+}
+
